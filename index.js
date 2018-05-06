113f9ee9010b0402ae689db00f9e338840faf99c
Lisa Mac <lisamac421@gmail.com>
2018-05-06 22:46:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function appendKitten(name){
+  var moreKittens = [...kittens, name]
+  return moreKittens
+}
+
+function prependKitten(name){
+  var prependedKittens = [name, ...kittens]
+  return prependedKittens
+}
+
+function removeLastKitten(){
+  var noLastKittens = kittens.slice(-1)
+  return noLastKittens
+}
