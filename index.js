fdb832e9898ff20eb5d00b7779375f7e90cc8ff5
Lisa Mac <lisamac421@gmail.com>
2018-05-06 22:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  var noLastKittens = kittens.slice(-1)
+  var noLastKittens = kittens.slice(0, length - 1)
   return noLastKittens
-}
+  
